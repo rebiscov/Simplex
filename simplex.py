73381803a2c5d674eea5138185494c88fc715e2f
REBISCOUL <vincent.rebiscoul@ens-lyon.fr>
2017-11-05 17:42:07 +0100

    fixed confusion between m and n in print_lp(), begin to code class Tableau

--- simplex.py
+++ simplex.py
@@ -12,13 +12,13 @@ class Lp:
         self.c = objective_vector
 
     def print_lp(self):
-        lp = [["" for j in range(0, self.m+3)] for i in range(0, self.n+2)]
+        lp = [["" for j in range(0, self.n+3)] for i in range(0, self.m+2)]
 
         lp[0][0] = "Maximize"
         lp[1][0] = "Such that"
 
-        for i in range(1, self.n+1):
-            for j in range(1, self.m+1):
+        for i in range(1, self.m+1):
+            for j in range(1, self.n+1):
                 if j == 1:
                     x = ""
                 else:
@@ -26,7 +26,7 @@ class Lp:
                     
                 lp[i][j] = x + str(self.a[i-1,j-1]) + "x_" + str(j)
 
-        for j in range(1, self.m+1):
+        for j in range(1, self.n+1):
             if j == 1:
                 x = ""
             else:
@@ -34,13 +34,33 @@ class Lp:
             
             lp[0][j] = x + str(self.c[j-1]) + "x_" + str(j)
 
-        for i in range(1, self.n+1):
+        for i in range(1, self.m+1):
             lp[i][-1] = "<= " + str(self.b[i-1])
                 
         print(tabulate(lp))
 
-
+class Tableau():
+    """ Structure which represents a tableau """
+    
+    def __init__(self, lp):
+        """ Convert a linear programm to a tableau and add slack variables """
         
+        self.n = lp.m + 1
+        self.m = lp.n + lp.m + 1
+        
+        self.tab = np.empty((self.n, self.m), dtype=Fraction)
+
+        print(lp.a)
+        print(lp.n, lp.m)
+
+        for i in range(0, self.n):
+            for j in range(0, self.m):
+                self.tab[i, j] = Fraction(0,1)
+
+        for i in range(1, lp.m+1):
+            for j in range(0, lp.n):
+                print((i, j))
+                self.tab[i,j] = lp.a[i-1, j]
 
 
 def parse_e(e):
@@ -75,9 +95,10 @@ def parse_lp(filename):
 
         print("PARSING FINISHED.")
 
-        return Lp(m, n, np.array(a), np.array(b), np.array(c))
+        return Lp(n, m, np.array(a), np.array(b), np.array(c))
     
 
 lin = parse_lp("linear_problem.in")
 lin.print_lp()
-
+t = Tableau(lin)
+print(tabulate(t.tab))
